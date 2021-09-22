6406009a6bd3bf5c1e1ea673998697fe7827c61b
Carlos Ramos <skhcri@gmail.com>
2021-09-23 01:27:29 +0200

    Update terminado

--- tema1/funciones.php
+++ tema1/funciones.php
@@ -1,23 +1,39 @@
-<?php
+<?php declare(strict_types=1);
 
 $tabla = "alumnos";
-$campos = array("nombre" => "Saúl", "apellidos" => "Pacheco");
+$campos = array("id" => 1, "nombre" => "Saúl", "apellidos" => "Pacheco");
 
 
-function insert ($campos) {
+function insert ($tabla, $campos) :string {
     $query = "";
     
     foreach ($campos as $campo => $valor) {
-        $query .= "insert into ($campo, $valor) values (:$campo :$valor)</br>";
-
+        $query .= "insert into $tabla ($campo, $valor) values (:$campo :$valor)</br>";
     }
     
     return $query;
-
-
 }
 
-echo insert ($campos);
+function insertReferencia (&$tabla, &$campos):string {
+    $query = "";
 
+    foreach ($campos as $campo => $valor) {
+        $query .= "insert into $tabla ($campo, $valor) values (:$campo :$valor)</br>";
+    }
+
+    return $query;
+}
+
+function update (&$tabla, &$campos, $id) :string{
+    $query ="";
+    $query = "UPDATE $tabla SET nombre=:$campos[nombre], apellidos=:$campos[apellidos] WHERE id = :$campos[id]";
+
+    return $query;
+}
+
+echo insert ($tabla, $campos);
+echo $campos["nombre"];
+echo insertReferencia ($tabla, $campos);
+echo update ($tabla, $campos, 1);
 
 ?>
