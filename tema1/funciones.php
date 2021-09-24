106daf3bf4f1e796e9530821fe93c654dabf59d2
Carlos Ramos <skhcri@gmail.com>
2021-09-24 09:01:33 +0200

    insert Fix

--- tema1/funciones.php
+++ tema1/funciones.php
@@ -1,19 +1,31 @@
 <?php declare(strict_types=1);
 
 $tabla = "alumnos";
-$campos = array("id" => 1, "nombre" => "Saúl", "apellidos" => "Pacheco");
+$campos = array("nombre" => "Saúl", "apellidos" => "Pacheco");
+$campos2 = array("id" => 1, "nombre" => "Saúl", "apellidos" => "Pacheco");
 $num1;
 $num2;
 $operator ="";
 
 function insert ($tabla, $campos) :string {
-    $query = "";
-    
+    $query = "insert into $tabla (";
+    $queryAtributes ="";
+    $queryValuesText =") values (";
+    $queryValues ="";
+    $queryEnding= ")</br>";
+    $finalQuery ="";
+
+
     foreach ($campos as $campo => $valor) {
-        $query .= "insert into $tabla ($campo, $valor) values (:$campo :$valor)</br>";
+        $queryAtributes .= "$campo,";
+        $queryValues .= ":$campo,";
     }
+    $queryAtributes = substr($queryAtributes,0,-1);
+    $queryValues = substr($queryValues,0,-1);
     
-    return $query;
+    $finalQuery = $query.$queryAtributes.$queryValuesText.$queryValues.$queryEnding;
+
+    return $finalQuery;
 }
 
 function insertReferencia (&$tabla, &$campos):string {
@@ -27,7 +39,6 @@ function insertReferencia (&$tabla, &$campos):string {
 }
 
 function update (&$tabla, &$campos, $id) :string{
-    $query ="";
     $query = "UPDATE $tabla SET nombre=:$campos[nombre], apellidos=:$campos[apellidos] WHERE id = :$campos[id]";
 
     return $query;
@@ -49,8 +60,8 @@ $operation = function ($num1, $num2, $operator) {
 };
 
 echo insert ($tabla, $campos);
-echo insertReferencia ($tabla, $campos);
-echo update ($tabla, $campos, 1);
+echo insertReferencia ($tabla, $campos2);
+echo update ($tabla, $campos2, 1);
 echo "</br>";
 echo $operation(1,2,"+");
 
