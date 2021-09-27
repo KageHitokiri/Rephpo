ac127b909e8d356c8557e0873ec98bbf756f52db
Carlos Ramos <skhcri@gmail.com>
2021-09-27 09:52:03 +0200

    name added

--- tema1/cadenas.php
+++ tema1/cadenas.php
@@ -1,9 +1,19 @@
 <?php
 
 $url = "https";
+$nombre = $_GET["nombre"];
+$nombre = trim($nombre, "/");
 
 echo "Cantidad de carácteres: ".strlen($url)."<br>";
 echo "Mayusculas: ".strtoupper($url)."<br>";
 echo "Minusculas: ".strtolower($url)."<br>";
+
+if ($nombre == null) {
+    $nombre = "Carlos";
+}
+
+echo "Te llamas ".$nombre;
+
+echo parse_url();
 
 ?>
