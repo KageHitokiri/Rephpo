439d1d5aae417d6c3c05ec4b1dd983b50ce176e4
Carlos Ramos <skhcri@gmail.com>
2021-09-29 09:27:00 +0200

    prefix added

--- tema1/cadenas.php
+++ tema1/cadenas.php
@@ -1,18 +1,24 @@
 <?php
 
 $url = "https";
-$nombre = $_GET["nombre"];
+$nombre = $_GET["nombre"]??="";
+$prefijo = $_GET["prefijo"]??="";
 $nombre = trim($nombre, "/");
 
 echo "Cantidad de carácteres: ".strlen($url)."<br>";
 echo "Mayusculas: ".strtoupper($url)."<br>";
 echo "Minusculas: ".strtolower($url)."<br>";
 
-if ($nombre == null) {
+if ($nombre == "") {
     $nombre = "Carlos";
 }
 
+if ($prefijo == "") {
+    $prefijo = "Sin prefijo";
+}
+
 echo "Te llamas ".$nombre;
+echo $prefijo;
 
 echo parse_url();
 
