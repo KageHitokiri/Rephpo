439fbab97d20cd27ed8f704f9de3369d3a58b9a9
Carlos Ramos <skhcri@gmail.com>
2021-10-04 09:53:35 +0200

    sort2 finished

---
+++ tema1/sort2.php
@@ -0,0 +1,35 @@
+<?php
+
+$temperatures = "5,7,10,40,30,13,15,6,25,26,8";
+$counter = 0;
+$colder ="";
+$hotter ="";
+
+$tempArray = explode(",",$temperatures,-1);
+echo "<h4>Las 5 temperaturas más frias han sido:</h4>";
+sort($tempArray);
+foreach ($tempArray as $value) {
+    $colder .= $value."º, ";
+    $counter++;
+    if ($counter==5) {
+        break;
+    }
+}
+
+$colder = substr($colder,0, -2);
+echo $colder;
+
+echo "<h4>Las 5 temperaturas más cálidas</h4>";
+rsort($tempArray);
+
+foreach ($tempArray as $value) {
+    $hotter .= $value."º, ";
+    $counter--;
+    if ($counter==0) {
+        break;
+    }
+}
+$hotter = substr($hotter,0, -2);
+print_r($hotter);
+
+?>
