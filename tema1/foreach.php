a6b2f51e09771527675fb3bc4df013d031ee6cc0
Carlos Ramos <skhcri@gmail.com>
2021-10-04 08:56:15 +0200

    foreach done

---
+++ tema1/foreach.php
@@ -0,0 +1,17 @@
+<?php
+
+$colors = array('blanco','verde','rojo','azul');
+
+function createColorList($array) {
+    echo "<ul>";
+        foreach ($array as $value) {
+            echo "<li>$value</li>";
+        }
+    echo "<ul>";
+};
+
+echo createColorList($colors);
+
+
+
+?>
