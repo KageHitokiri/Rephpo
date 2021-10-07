bc6c8b1a005d84551bb92e737d924f1c41ee47ef
Carlos Ramos <skhcri@gmail.com>
2021-10-07 16:21:56 +0200

    cambio de foreach a funciones de array

--- tema1/sort2.php
+++ tema1/sort2.php
@@ -1,35 +1,27 @@
 <?php
-
+ 
 $temperatures = "5,7,10,40,30,13,15,6,25,26,8,1,3";
 $counter = 0;
 $colder ="";
+$avgTemp="";
 $hotter ="";
-
+ 
 $tempArray = explode(",",$temperatures);
+$avgTemp = array_sum($tempArray)/count($tempArray);
 echo "<h4>Las 5 temperaturas más frias han sido:</h4>";
-sort($tempArray);
-foreach ($tempArray as $value) {
-    $colder .= $value."º, ";
-    $counter++;
-    if ($counter==5) {
-        break;
-    }
-}
-
-$colder = substr($colder,0, -2);
-echo $colder;
-
+$colder = $tempArray;
+sort($colder);
+$colder = array_slice($colder,0,5);
+print_r ($colder);
+ 
 echo "<h4>Las 5 temperaturas más cálidas</h4>";
 rsort($tempArray);
-
-foreach ($tempArray as $value) {
-    $hotter .= $value."º, ";
-    $counter--;
-    if ($counter==0) {
-        break;
-    }
-}
-$hotter = substr($hotter,0, -2);
 print_r($hotter);
-
+$hotter = $tempArray;
+rsort($hotter);
+$hotter = array_slice($hotter,0,5);
+print_r ($hotter);
+ 
+echo "<h4>La temperatura media es: </h4>$avgTemp";
+ 
 ?>
