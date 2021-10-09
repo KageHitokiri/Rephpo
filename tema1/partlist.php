6248666de6ae8bc05169bcd98cb59c1f822b53a2
Carlos Ramos <skhcri@gmail.com>
2021-10-09 11:05:58 +0200

    partlist finished

--- tema1/partlist.php
+++ tema1/partlist.php
@@ -1,7 +1,17 @@
 <?php
     $message = ["Seguro","que","apruebo","esta","asignatura"];
+    $array1 = [];
 
-    print_r($message);
+    function partlist($originalArray, &$array1) {
+        for ($i = 0;$i<count($originalArray)-1; $i++) {
     
+            $array1 [$i][0]= implode(array_slice($originalArray,0,$i+1)," ");
+            $array1 [$i][1]= implode(array_slice($originalArray,$i+1,count($originalArray))," ");
+        }
+        
+    }
+    
+    partlist($message,$array1);    
+    print_r($array1);
 
 ?>
