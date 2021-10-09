c96f772249cc04cb168b0a8df483a7e3491b02d7
Carlos Ramos <skhcri@gmail.com>
2021-10-09 12:09:21 +0200

    callback finished (minor bug detected)

--- tema1/callback.php
+++ tema1/callback.php
@@ -1,5 +1,12 @@
 <?php
+    $words = ["Esto","es","una","prueba"];
+    $minmaxArray;    
 
-    //todo
+    function lenCount($value) {
+        return iconv_strlen($value);
+    }
+
+    $minmaxArray = array_map(lenCount,$words);
+    echo "El valor mínimo es: ".min($minmaxArray)."<br>Y el valor máximo es: ".max($minmaxArray);
 
 ?>
