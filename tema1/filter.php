023e34f6a765469a132fa9b69ab8a505975eb3f7
Carlos Ramos <skhcri@gmail.com>
2021-10-11 08:28:52 +0200

    minor fixes, filter not finished

--- tema1/filter.php
+++ tema1/filter.php
@@ -1,5 +1,31 @@
-<?php
+<?php          
+    $contactos = array(
+        array("codigo" => 1, "nombre" => "Juan Pérez",
+        "telefono" => "966112233", "email" => "juanp@gmail.com"),
+        array("codigo" => 2, "nombre" => "Ana López",
+        "telefono" => "965667788", "email" => "anita@hotmail.com"),
+        array("codigo" => 3, "nombre" => "Mario Montero",
+        "telefono" => "965929190", "email" => "mario.mont@gmail.com"),
+        array("codigo" => 4, "nombre" => "Laura Martínez",
+        "telefono" => "611223344", "email" => "lm2000@gmail.com"),
+        array("codigo" => 5, "nombre" => "Nora Jover",
+        "telefono" => "638765432", "email" => "norajover@hotmail.com"),
+        );
+    $domFilter = strtolower($_GET["dominio"]??=""); 
+    $test ="";
 
-    //todo
+    echo $domFilter;
+    function arrayFilter($contactos){
+        
+    }
+
+    foreach ($contactos as $campo => $valor) {
+        echo $campo;
+        foreach ($valor as $es => $est) {
+            $test .=$est;
+        }
+    }
+
+    echo $test;
 
 ?>
