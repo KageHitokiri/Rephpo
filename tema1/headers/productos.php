7b427dc441d354fe05ba66a19221c45f6f65a52a
Carlos Ramos <skhcri@gmail.com>
2021-10-11 09:39:20 +0200

    productos finished

--- tema1/headers/productos.php
+++ tema1/headers/productos.php
@@ -1,5 +1,17 @@
 <?php
+    $productID = $_GET['id']??="5";
+    $products = [
+        ['id'=>0, 'nombre' => "Daga", "precio" => 10],
+        ['id'=>1, 'nombre' => "Espada", "precio" => 15],
+        ['id'=>2, 'nombre' => "Espadón", "precio" => 50],
+        ['id'=>3, 'nombre' => "Hacha", "precio" => 25],
+        ['id'=>4, 'nombre' => "Arco", "precio" => 35]        
+    ];
 
-//todo
-
+    if (array_key_exists($productID,$products)) {
+        echo "Está";
+    } else {
+        http_response_code(404);
+        echo "<h1>ERROR 404</h1>";
+    }
 ?>
