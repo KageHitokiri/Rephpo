cbd39a8e42deef064a5aa921c8904dd5ca525c0e
Carlos Ramos <skhcri@gmail.com>
2021-10-11 08:38:26 +0200

    reestructuración

---
+++ tema1/headers/idioma.php
@@ -0,0 +1,55 @@
+<?php
+
+    $language = "";
+
+    //Crea aquí tu script para seleccionar el idioma
+
+    
+
+    //Fin script
+
+    if ($language == "en"){
+
+        $content = "This page is in English";
+
+        $title = "Change the language of the page";
+
+    }else{
+
+        $content = "Esta página está en Castellano (Idioma por defecto)";
+
+        $title = "Cambiar el idioma de la página";
+
+    }
+
+?>
+
+<!doctype html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title><?= $title ?></title>
+
+    <meta name="author" content="Carlos Ramos">
+
+</head>    
+
+<body>
+
+    <ul><?= $title ?>
+
+        <li><a href='idioma.php?setLanguage=es'>Español</a></li>
+
+        <li><a href='idioma.php?setLanguage=en'>Inglés</a></li>
+
+    </ul>
+
+    <?= $content ?>
+
+</body>
+
+</html>
