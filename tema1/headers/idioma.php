fadc34c9799e86a9a1ffa0d1e6c3a8f2299ab8bb
Carlos Ramos <skhcri@gmail.com>
2021-10-11 08:53:02 +0200

    header_idioma finished

--- tema1/headers/idioma.php
+++ tema1/headers/idioma.php
@@ -1,55 +1,33 @@
 <?php
 
-    $language = "";
-
-    //Crea aquí tu script para seleccionar el idioma
-
-    
-
-    //Fin script
+    if (substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2)=="es") {
+        $language = "es";
+    } else {
+        $language = "en";
+    }    
 
     if ($language == "en"){
-
         $content = "This page is in English";
-
         $title = "Change the language of the page";
-
     }else{
-
         $content = "Esta página está en Castellano (Idioma por defecto)";
-
         $title = "Cambiar el idioma de la página";
-
     }
 
 ?>
 
 <!doctype html>
-
 <html lang="es">
-
 <head>
-
     <meta charset="utf-8">
-
     <title><?= $title ?></title>
-
     <meta name="author" content="Carlos Ramos">
-
 </head>    
-
 <body>
-
     <ul><?= $title ?>
-
         <li><a href='idioma.php?setLanguage=es'>Español</a></li>
-
         <li><a href='idioma.php?setLanguage=en'>Inglés</a></li>
-
     </ul>
-
     <?= $content ?>
-
 </body>
-
 </html>
